58e09791e2e2a64ccb496a21c2de02e57757e30a
Veljko Popović <veljko_popovic_90@hotmail.com>
2023-09-21 13:51:52 +0200

    slider

--- js/build.js
+++ js/build.js
@@ -1,3 +1,8 @@
+/* eslint-disable max-len */
+/* eslint-disable new-cap */
+/* eslint-disable max-depth */
+/* eslint-disable max-statements */
+/* eslint-disable no-eval */
 Fliplet.Widget.instance({
   name: 'conditional-container',
   displayName: 'Conditional container',
@@ -9,159 +14,176 @@ Fliplet.Widget.instance({
     {
       name: 'dt-content',
       displayName: 'Drag&drop area',
-      // placeholder: '<div class="well text-center" data-view="content">Add components here.</div>'
       placeholder: '<div class="well text-center">Add components here.</div>'
     }
   ],
   render: {
     template: [
-      // '<div class="placeholder"><i class="icon fl-icon-settings"></i>{! placeholder !}</div>',
       '<div class="conditional" data-view="dt-content"></div>'
     ].join(''),
-    beforeReady: function () {
+    beforeReady: function() {
       let element = $(this.$el);
-      Fliplet.Env.get('interact') ? element.addClass('edit') : element.removeClass('edit');
+
+      Fliplet.Env.get('interact')
+        ? element.addClass('edit') : element.removeClass('edit');
     },
-    ready: async function () {
+    ready: async function() {
       await Fliplet.Widget.initializeChildren(this.$el, this);
+
+      function ifArray(elem) {
+        if (Array.isArray(elem)) {
+          return 'array';
+        } else if (typeof elem === 'string') {
+          return 'string';
+        }
+
+        return false;
+      }
+
+      function decodeHTMLEntities(str) {
+        var temp = document.createElement('div');
+
+        temp.style.display = 'none';
+        temp.innerHTML = str;
+
+        return temp.textContent || temp.innerText;
+      }
+
+      function ifValidJson(elem) {
+        try {
+          JSON.parse(elem);
+
+          let parsedValue = JSON.parse(elem);
+
+          if (Array.isArray(parsedValue)) {
+            return 'array';
+          }
+
+          return false;
+        } catch (error) {
+          return 'string';
+        }
+      }
+
+      function evaluate(condition, expression) {
+        try {
+          eval(expression);
+
+          if (eval(expression)) {
+            return condition['visibility'];
+          }
+        } catch (error) {
+          // console.log('Expression can\'t be evaluated. Error: ' + error);
+
+          return false;
+        }
+      }
+
+      function setResult(expr) {
+        if (expr) {
+          return expr === 'hide' ? false : true;
+        }
+
+        return false;
+      }
+
+      function ifArrayIncludes(array, condition) {
+        if (array.includes(condition['user_value'])) {
+          setResult(condition['visibility']);
+        } else {
+          _.forEach(array, function(value) {
+            if (typeof value === 'number') {
+              // user value to number
+              if (value === +condition['user_value']) {
+                setResult(condition['visibility']);
+              }
+            }
+          });
+        }
+      }
 
       let helper = this;
       let conditions = this.fields.conditions;
       let environment = Fliplet.Env.get('preview');
-      $(helper.el).addClass('hidden'); //by default button is hidden
 
-      return Fliplet.Session.get().then(function onSessionRetrieved(session) {
-        if (session && session.entries) {
-          if (session.entries.dataSource) {
-            let user = session.entries.dataSource.data;
-            let result;
+      $(helper.el).addClass('hidden'); // by default button is hidden
 
-            function ifArray(elem) {
-              if (Array.isArray(elem)) {
-                return 'array';
-              } else if (typeof elem === 'string') {
-                return 'string';
-              } else { return false; }
-            }
+      return Fliplet.Session.get()
+        .then(function onSessionRetrieved(session) {
+          if (session && session.entries) {
+            if (session.entries.dataSource) {
+              let user = session.entries.dataSource.data;
+              let result;
 
-            function ifValidJson(elem) {
-              try {
-                JSON.parse(elem);
-                let parsedValue = JSON.parse(elem);
-                if (Array.isArray(parsedValue)) {
-                  return 'array';
-                }
-                return false;
-              } catch (error) {
-                return 'string';
-              }
-            }
+              if (conditions) {
+                for (let i = 0; i < conditions.length; i++) {
+                  if (user.hasOwnProperty(conditions[i]['user_key'])) {
+                    let expression;
+                    let logic = conditions[i]['logic'];
 
-            function evaluate(condition, expression) {
-              try {
-                eval(expression);
-                if (eval(expression)) {
-                  return condition['visibility'];
-                }
-              } catch (error) {
-                console.log('Expression can\'t be evaluated. Error: ' + error);
-                return false;
-              }
-            }
+                    if (logic !== 'contains') {
+                      expression = '"' + user[conditions[i]['user_key']] + '"';
 
-            function setResult(expr) {
-              if (expr) {
-                result = expr === 'hide' ? false : true;
-              }
-            }
-
-            function ifArrayIncludes(array, condition) {
-              if (array.includes(condition['user_value'])) {
-                setResult(condition['visibility']);
-              } else {
-                _.forEach(array, function (value) {
-                  if (typeof value === 'number') {
-                    //user value to number
-                    if (value === +condition['user_value']) {
-                      setResult(condition['visibility']);
-                    }
-                  }
-                });
-              }
-            }
-
-            if (conditions) {
-              for (let i = 0; i < conditions.length; i++) {
-                if (user.hasOwnProperty(conditions[i]['user_key'])) {
-                  let expression;
-                  let logic = conditions[i]['logic'];
-
-                  if (logic !== 'contains') {
-                    expression = '"' + user[conditions[i]['user_key']] + '"';
-                    if (logic === 'equal') {
-                      expression += ' === ' + '"' + conditions[i]['user_value'] + '"';
-                    } else if (logic === 'not-equal') {
-                      expression += ' !== ' + '"' + conditions[i]['user_value'] + '"';
-                    }
-                    /*else if (logic === 'starts'){
+                      if (logic === 'equal') {
+                        expression += ' === ' + '"' + conditions[i]['user_value'] + '"';
+                      } else if (logic === 'not-equal') {
+                        expression += ' !== ' + '"' + conditions[i]['user_value'] + '"';
+                      }
+                      /* else if (logic === 'starts'){
                       expression += '.indexOf("' + conditions[i]['user_value'] + '") === 0';
                     } else if (logic === 'ends'){
                       expression +=  '.endsWith("' + conditions[i]['user_value'] + '")';
                     }*/
 
-                    setResult(evaluate(conditions[i], expression));
-
-                  } else {
-                    let keyType = ifArray(user[conditions[i]['user_key']]);
-                    if (!keyType) {
-                      //other type but array or string
-                      expression = '"' + user[conditions[i]['user_key']] + '".indexOf("' + conditions[i]['user_value'] + '") > -1';
                       setResult(evaluate(conditions[i], expression));
-                    } else if (keyType === 'string') {
-                      //check if string can be parsed into JSON array
-                      let ifJSON = ifValidJson(user[conditions[i]['user_key']]);
-                      if (!ifJSON || ifJSON === 'string') {
-                        //parsed value is not an array nor a string
-                        // expression = '"' + user[conditions[i]['user_key']] + '".indexOf("' + conditions[i]['user_value'] + '") > -1';
-                        expression = user[conditions[i]['user_key']]
-                          .split(',')
-                          .map(el => el.trim())
-                          .includes(decodeHTMLEntities(conditions[i]['user_value']));
+                    } else {
+                      let keyType = ifArray(user[conditions[i]['user_key']]);
+
+                      if (!keyType) {
+                      // other type but array or string
+                        expression = '"' + user[conditions[i]['user_key']] + '".indexOf("' + conditions[i]['user_value'] + '") > -1';
                         setResult(evaluate(conditions[i], expression));
-                      } else if (ifJSON === 'array') {
-                        let currentArray = JSON.parse(user[conditions[i]['user_key']]);
-                        ifArrayIncludes(currentArray, conditions[i]);
+                      } else if (keyType === 'string') {
+                      // check if string can be parsed into JSON array
+                        let ifJSON = ifValidJson(user[conditions[i]['user_key']]);
+
+                        if (!ifJSON || ifJSON === 'string') {
+                        // parsed value is not an array nor a string
+                          expression = user[conditions[i]['user_key']]
+                            .split(',')
+                            .map(el => el.trim())
+                            .includes(decodeHTMLEntities(conditions[i]['user_value']));
+                          setResult(evaluate(conditions[i], expression));
+                        } else if (ifJSON === 'array') {
+                          let currentArray = JSON.parse(user[conditions[i]['user_key']]);
+
+                          ifArrayIncludes(currentArray, conditions[i]);
+                        }
+                      } else if (keyType === 'array') {
+                        ifArrayIncludes(user[conditions[i]['user_key']], conditions[i]);
                       }
-                    } else if (keyType === 'array') {
-                      ifArrayIncludes(user[conditions[i]['user_key']], conditions[i]);
                     }
-                  }
-                } else {
-                  if (environment) {
+                  } else if (environment) {
                     Fliplet.UI.Toast('User doesn\'t contain key: ' + conditions[i]['user_key']);
                   }
                 }
               }
-            }
 
-            if (result) {
-              $(helper.el).removeClass('hidden');
-            }
-          } else {
-            if (environment) {
+              if (result) {
+                $(helper.el).removeClass('hidden');
+              }
+            } else if (environment) {
               Fliplet.UI.Toast('User is not logged in');
             }
-          }
-        } else {
-          if (environment) {
+          } else if (environment) {
             Fliplet.UI.Toast('User is not logged in');
           }
-        }
-        return Promise.resolve(true);
-      });
+
+          return Promise.resolve(true);
+        });
     },
-    change: function (arrayOfFields) {
-      _.forEach(arrayOfFields, function (field) {
+    change: function(arrayOfFields) {
+      _.forEach(arrayOfFields, function(field) {
         updatePanelTitleName(field);
       });
 
@@ -174,16 +196,18 @@ Fliplet.Widget.instance({
         if (visibility && key && condition && value) {
           visibility = visibility.charAt(0).toUpperCase() + visibility.slice(1);
           condition = condition.charAt(0).toLowerCase() + condition.slice(1);
+
           if (condition.indexOf('equal') > -1) {
             condition += ' to';
           }
 
           let titleName = `${visibility} if "${key}" ${condition} "${value}"`;
+
           field[0].value = titleName;
         } else {
           field[0].value = 'Incomplete condition';
         }
       }
     }
-  },
+  }
 });
